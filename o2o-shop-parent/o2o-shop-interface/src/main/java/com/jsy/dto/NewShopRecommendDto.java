7319495401632eca93f6815afd1f4ee024223879
wodemao <1973982151@qq.com>
2021-11-15 14:38:29 +0800

    修改Shop返回参数

---
+++ o2o-shop-parent/o2o-shop-interface/src/main/java/com/jsy/dto/NewShopRecommendDto.java
@@ -0,0 +1,34 @@
+package com.jsy.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="C端店铺信息", description="C端展示店铺信息返回")
+public class NewShopRecommendDto implements Serializable {
+    @ApiModelProperty(value = "店铺名称")
+    private String shopName;
+
+    @ApiModelProperty(name = "shopTreeIdName", value = "门店类型名称")
+    private String shopTreeIdName;
+
+    private Long id;
+
+    @ApiModelProperty(value = "商品名称/服务标题")
+    private String title;
+
+    @ApiModelProperty(value = "商品价格")
+    private BigDecimal price;
+
+    @ApiModelProperty(value = "评分")
+    private Float grade;
+
+    @ApiModelProperty(value = "距离多远")
+    private String distance;
+}
