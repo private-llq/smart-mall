970b74d5da650d22cf7417b0950bd144178e2d0d
wodemao <1973982151@qq.com>
2021-11-17 10:02:26 +0800

    新增c端用户个人档案查询

--- o2o-goods-parent/o2o-goods-service/src/main/java/com/jsy/controller/SetMenuController.java
+++ o2o-goods-parent/o2o-goods-service/src/main/java/com/jsy/controller/SetMenuController.java
@@ -16,10 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.jsy.basic.util.vo.CommonResult;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
