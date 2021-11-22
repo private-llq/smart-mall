534c5fe6afd2d82ccfa757c003833e9057d90c6c
lijin <1192323939@qq.com>
2021-11-22 10:41:16 +0800

    解决springcloud版本冲突  -- 刘金荣

---
+++ o2o-user-parent/o2o-user-interface/src/main/java/com/jsy/domain/UserAddr.java
@@ -0,0 +1,64 @@
+package com.jsy.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 用户地址管理表	
+ * </p>
+ *
+ * @author yu
+ * @since 2021-11-22
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("w_user_addr")
+@ApiModel(value="UserAddr对象", description="用户地址管理表	")
+public class UserAddr implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "主键")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "联系人")
+    private String linkman;
+
+    @ApiModelProperty(value = "性别    0  男  |  1 女")
+    private Integer sex;
+
+    @ApiModelProperty(value = "电话")
+    private String pone;
+
+    @ApiModelProperty(value = "地区")
+    private String district;
+
+    @ApiModelProperty(value = "详细地址")
+    private String detailedAddress;
+
+    @ApiModelProperty(value = "标签 0 家 | 1 公司 | 2 学校 ")
+    private String tag;
+
+    @ApiModelProperty(value = "默认地址")
+    private String defaultAddress;
+
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private LocalDateTime updateTime;
+
+    @ApiModelProperty(value = "逻辑删除   0 正常 |   1 删除")
+    private Long deleted;
+
+
+}
---
+++ o2o-user-parent/o2o-user-service/src/main/java/com/jsy/service/impl/UserAddrServiceImpl.java
@@ -0,0 +1,92 @@
+package com.jsy.service.impl;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jsy.basic.util.PageInfo;
+import com.jsy.basic.util.utils.BeansCopyUtils;
+import com.jsy.domain.UserAddr;
+import com.jsy.dto.UserAddrDto;
+import com.jsy.mapper.UserAddrMapper;
+import com.jsy.param.UserAddrParam;
+import com.jsy.query.UserAddrQuery;
+import com.jsy.service.IUserAddrService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 用户地址管理表	 服务实现类
+ * </p>
+ *
+ * @author yu
+ * @since 2021-11-22
+ */
+@Service
+public class UserAddrServiceImpl extends ServiceImpl<UserAddrMapper, UserAddr> implements IUserAddrService {
+
+    @Autowired
+    private UserAddrMapper useraddrmapper;
+
+
+    /**
+     * 用户添加地址
+     * @param userAddrParam
+     * @return
+     */
+    @Override
+    @Transactional
+    public void addUserAddr(UserAddrParam userAddrParam) {
+        UserAddr userAddr = new UserAddr();
+        BeanUtils.copyProperties(userAddrParam,userAddr);
+        useraddrmapper.insert(userAddr);
+
+    }
+
+    /**
+     * 用户修改地址
+     * @param userAddrParam
+     * @return
+     */
+    @Override
+    @Transactional
+    public void updateUserAddr(UserAddrParam userAddrParam) {
+        UserAddr userAddr = new UserAddr();
+        BeanUtils.copyProperties(userAddrParam,userAddr);
+        useraddrmapper.updateById(userAddr);
+    }
+
+    /**
+     * 用户删除地址
+     * @param id
+     */
+    @Override
+    @Transactional
+    public void deleteUserAddr(Long id) {
+        useraddrmapper.deleteById(id);
+    }
+
+    /**
+     * 地址分页列表
+     * @param userAddrQuery 查询对象
+     * @return  分页对象
+     */
+    @Override
+    public PageInfo<UserAddrDto> UserAddrPageList(UserAddrQuery userAddrQuery) {
+        Page<UserAddr> page = new Page<>(userAddrQuery.getPage(),userAddrQuery.getRows());
+        Page<UserAddr> userAddrPage = useraddrmapper.selectPage(page, null);
+        List<UserAddr> records = userAddrPage.getRecords();
+        List<UserAddrDto> userAddrDtos = BeansCopyUtils.listCopy(records, UserAddrDto.class);
+        PageInfo<UserAddrDto> pageInfo = new PageInfo<>();
+        pageInfo.setSize(userAddrPage.getSize());
+        pageInfo.setRecords(userAddrDtos);
+        pageInfo.setCurrent(userAddrPage.getCurrent());
+        pageInfo.setTotal(userAddrPage.getTotal());
+        return pageInfo;
+    }
+
+
+
+}
