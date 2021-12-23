dcf7ad629da75adbcd7d816d003806db9821cef5
wodemao <1973982151@qq.com>
2021-12-23 16:25:39 +0800

    修改医疗机构查询店铺

--- o2o-shop-parent/o2o-shop-service/src/main/java/com/jsy/service/impl/NewShopServiceImpl.java
+++ o2o-shop-parent/o2o-shop-service/src/main/java/com/jsy/service/impl/NewShopServiceImpl.java
@@ -614,20 +614,26 @@ public class NewShopServiceImpl extends ServiceImpl<NewShopMapper, NewShop> impl
         List<NewShop> newShops= shopMapper.getMedicalShop(shopQuery);
         List<NewShopRecommendDto> recommendDtoList = new ArrayList<>();
         Integer number = 0;
-        for (NewShop newShop : newShops) {
-            if (number>=5){
-                break;
+        if (newShops.size()>0){
+            for (NewShop newShop : newShops) {
+                if (number>=5){
+                    break;
+                }
+                number++;
+                NewShopRecommendDto recommendDto = new NewShopRecommendDto();
+                System.out.println("ddddddddddddddddddddddddddddddddddd  + number "+newShop+number);
+                if (ObjectUtil.isNotNull(newShop)){
+                    BeanUtils.copyProperties(newShop,recommendDto);
+                    recommendDto.setDistance(newShop.getDistance().divide(new BigDecimal(1000).setScale(2, ROUND_HALF_UP)));
+                    String[] spilt = newShop.getShopTreeId().split(",");
+                    String shopTreeIdName = getShopTreeIdName(spilt);
+                    recommendDto.setShopTreeIdName(shopTreeIdName);
+                    recommendDtoList.add(recommendDto);
+                }
+                //.divide(new BigDecimal(1000)
             }
-            number++;
-            NewShopRecommendDto recommendDto = new NewShopRecommendDto();
-            BeanUtils.copyProperties(newShop,recommendDto);
-            //.divide(new BigDecimal(1000)
-            recommendDto.setDistance(newShop.getDistance().divide(new BigDecimal(1000).setScale(2, ROUND_HALF_UP)));
-            String[] spilt = newShop.getShopTreeId().split(",");
-            String shopTreeIdName = getShopTreeIdName(spilt);
-            recommendDto.setShopTreeIdName(shopTreeIdName);
-            recommendDtoList.add(recommendDto);
         }
+
         serviceDto.setShopList(recommendDtoList);
         NearTheServiceQuery serviceQuery = new NearTheServiceQuery();
         serviceQuery.setLatitude(shopQuery.getLatitude());
